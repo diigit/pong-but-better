367bd24880605cac27b752af34afd522fe2ac1b6
diigit <72575705+diigit@users.noreply.github.com>
2026-04-12 19:29:04 -0400

    style: update app title box and version in App.tsx

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -3,12 +3,10 @@ import { GameSettings } from "./components/ui/GameSettings";
 
 export function App() {  
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center gap-10 p-10">
-      <div className="flex-1 flex items-center flex-col">
-        <div className="p-4 rounded-xl bg-white/10 drop-shadow-2xl border-white/15 border-2">
-          <p className="font-title font-black text-4xl text-title text-shadow-2xs">pong-but-better</p>
-          <p className="font-light italic">version 0.0.0</p>
-        </div>
+    <div className="w-screen h-screen flex flex-col items-center gap-10 p-10">
+      <div className="flex-1 flex items-center flex-col justify-end">
+          <p className="font-sans font-bold text-4xl text-title text-shadow-sm">Pong, but Better</p>
+          <p className="font-light text-black/50 italic text-center">version 1.0.0</p>
       </div>
       <GameWindow/>
       <GameSettings/>
