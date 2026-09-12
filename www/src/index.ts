bedb6cb464a469f0d4f2e1cd4962571ac3fb8ecd
diigit <gunnsdc@gmail.com>
2026-09-12 20:21:18 +0000

    fix: ensure bun is using the wasm-pong-but-better package

--- www/src/index.ts
+++ www/src/index.ts
@@ -6,28 +6,6 @@ const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
-
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
   },
 
   development: process.env.NODE_ENV !== "production" && {
@@ -40,6 +18,3 @@ const server = serve({
 });
 
 console.log(`🚀 Server running at ${server.url}`);
-
-// load wasm thingamajig
-let a = wasm;
