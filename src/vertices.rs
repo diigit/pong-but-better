27aa8c853ae6b036e39200c896c9371e2865d3a8
diigit <gunnsdc@gmail.com>
2026-09-17 18:36:13 +0000

    feat: sending vertices to typescript from tessellation system

--- src/vertices.rs
+++ src/vertices.rs
@@ -1,11 +1,17 @@
 use deref::{Deref, DerefMut};
-use lyon::{math::Point, path::{builder::NoAttributes}, tessellation::{
-    BuffersBuilder, FillBuilder, FillOptions, FillTessellator, VertexBuffers, geometry_builder::{Positions, simple_builder},
-}};
-use specs::{Component, prelude::*};
+use lyon::{
+    math::Point,
+    path::builder::NoAttributes,
+    tessellation::{
+        geometry_builder::{simple_builder, Positions},
+        BuffersBuilder, FillBuilder, FillOptions, FillTessellator, VertexBuffers,
+    },
+};
+use specs::{prelude::*, Component};
 
 use crate::{
     physics::{Bounds, Position},
+    send_vertices,
     shapes::Shape,
 };
 
@@ -28,26 +34,33 @@ pub struct TessellationSystemData<'a> {
     position: ReadStorage<'a, Position>,
     shape: ReadStorage<'a, ShapeComp>,
     bounds: ReadStorage<'a, Bounds>,
-    vertices: Write<'a, Vertices>,
+    buf: Write<'a, Vertices>,
 }
 
 impl<'a> System<'a> for Tessellation {
     type SystemData = TessellationSystemData<'a>;
 
     fn run(&mut self, mut data: Self::SystemData) {
-        data.vertices.clear();
-        
-        let mut geo_builder: BuffersBuilder<'_, Point, u16, Positions> = simple_builder(&mut **data.vertices);
+        data.buf.clear();
+
+        let mut geo_builder: BuffersBuilder<'_, Point, u16, Positions> =
+            simple_builder(&mut **data.buf);
         let mut tessellator: FillTessellator = FillTessellator::new();
-		let fill_opts: FillOptions = FillOptions::tolerance(0.1);
-        let mut builder: NoAttributes<FillBuilder<'_>> = tessellator.builder(&fill_opts, &mut geo_builder);
+        let fill_opts: FillOptions = FillOptions::tolerance(0.1);
+        let mut builder: NoAttributes<FillBuilder<'_>> =
+            tessellator.builder(&fill_opts, &mut geo_builder);
 
         for (shape, position, bounds, _) in
-                (&data.shape, &data.position, &data.bounds, &data.visible).join()
-            {
-                shape.write_vertices(&mut builder, **position, **bounds);
-            }
+            (&data.shape, &data.position, &data.bounds, &data.visible).join()
+        {
+            shape.write_vertices(&mut builder, **position, **bounds);
+        }
 
-		    let _ = builder.build();  
+        if let Ok(()) = builder.build() {
+            send_vertices(
+                data.buf.vertices.as_ptr() as *const f32,
+                data.buf.vertices.len(),
+            );
+        }
     }
 }
